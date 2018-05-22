eb74c718a09987d05dc0666189c56330e3eaed64
DominikBurkert <dominik.al.burkert@gmail.com>
2018-05-22 11:24:48 +0200

    Mein Profil, bild hochladen(geht noch nicht), ohne Datenbank Speicherung

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,33 @@ Route::get('/suchen', 'HomeController@suchen')->name('suchen');
 Route::get('/erstellen', 'HomeController@erstellen')->name('erstellen');
 
 Route::get('/profil', 'HomeController@profil')->name('profil');
+
+Route::get('/', function () {
+    return view('EasyPlanHome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/erstellen', 'HomeController@erstellen')->name('erstellen');
+
+Route::get('/profil', 'HomeController@profil')->name('profil');
+
+
+Route::get('/profile', 'UserController@profile')->name('profile');
+Route::post('hochladen', 'UploadController@hochladen')->name('hochladen');
+
+Route::get('/suchen', function (){
+    $Vsuchen = \App\Veranstaltungen::all();
+
+    return View::make('Vsuchen')->with('Vsuchen', $Vsuchen);
+})->name('suchen');
+
+
+Route::get('/Veranstaltungen/{Name}', function ($name){
+   $VerNamen = \App\Veranstaltungen::where('name', $name)->firstOrFail();
+
+   return View::make('Vprofil')->with('name', $VerNamen);
+})->name('VProfil');
+
