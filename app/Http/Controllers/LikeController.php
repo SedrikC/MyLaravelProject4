9150ab7ad7a711a1c54595e4e7e44615ab8e13e6
se721cir <sedrik.cirzel@htwg-konstanz.de>
2018-06-12 02:15:40 +0200

    Commit

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\like;
+use Illuminate\Http\Request;
+use Auth;
+use DB;
+
+class LikeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $like = new like;
+        $Username = Auth::user();
+        $VerID = $request->input('VerID');
+        $like->Username = $Username->name;
+        $like->OptionID = $request->input('optionID');
+        $like->VeranstaltungID = $VerID;
+
+        $like->save();
+
+        return \Redirect::route('Veranstaltung', $VerID);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\like  $like
+     * @return \Illuminate\Http\Response
+     */
+    public function show(like $like)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\like  $like
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(like $like)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\like  $like
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, like $like)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\like  $like
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(like $like)
+    {
+        //
+    }
+}
