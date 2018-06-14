3d79c62aceaf3f5a5a99ced569a0efd59b04fde1
se721cir <sedrik.cirzel@htwg-konstanz.de>
2018-06-14 18:31:15 +0200

    Voyager raus und neue Dateien wieder rein

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
 {
 
     public function index(){
-        return view('profile', array('user' => Auth::user()));
+        return view('Profile', array('user' => Auth::user()));
     }
     public function update_avatar(Request $request){
 
